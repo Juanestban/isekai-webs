13cc3728fb7ba62bf9405212b518dbd17965386d
Juan Esteban <juanestban000@gmail.com>
2024-07-06 00:00:13 -0500

    first commit

---
+++ src/packages/mercado-libre/Button/Button.tsx
@@ -0,0 +1,50 @@
+import "./Button.css";
+
+interface ButtonProps {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary?: boolean;
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string;
+  /**
+   * How large should the button be?
+   */
+  size?: "small" | "medium" | "large";
+  /**
+   * Button contents
+   */
+  label: string;
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+const Button = ({
+  primary = false,
+  size = "medium",
+  backgroundColor,
+  label,
+  ...props
+}: ButtonProps) => {
+  const mode = primary ? "primary" : "secondary";
+  return (
+    <button
+      type="button"
+      className={["storybook-button", size, mode].join(" ")}
+      style={{ backgroundColor }}
+      {...props}
+    >
+      {label}
+    </button>
+  );
+};
+
+export { Button };
+export type { ButtonProps };
